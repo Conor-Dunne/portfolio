357c67e6a828b8c83c82f7b916352c7a61ead07e
Conor Dunne <conordunne@outlook.ie>
2023-03-10 14:40:30 +0900

    CSS

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,13 +4,13 @@ function ProjectCard({project}) {
     const {name, tools, description,image, live, code } = project;
     return (
         
-        <div className="flex flex-col gap-2 p-2 max-w-xs shadow-black shadow-sm hover:scale-110 transition-all ease-in-out cursor-pointer">
+        <div className="flex flex-col justify-between gap-2 p-2 max-w-xs tracking-wide bg-secondaryColor shadow-black shadow-sm hover:scale-110 transition-all ease-in-out cursor-pointer">
             <Fade>
             <img src={image} className="rounded" />
             <div>
                 <h2 className="text-2xl text-slate-300 font-bold">{name}</h2>
             </div>
-            <div className="h-16 p-3 text-sm text-slate-300 bg-secondaryColor rounded">
+            <div className="h-16 text-sm rounded">
                 {description}
             </div>
             <div className="font-mono text-accentColor">
