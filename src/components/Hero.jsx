b91ef8a58e23955179cc062a73cd313f214e3805
Conor Dunne <conordunne@outlook.ie>
2023-03-13 18:57:06 +0900

    CSS

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,8 @@ import { Link } from "react-scroll";
 
 function Hero() {
   return (
-    <div name="top" className="flex flex-wrap gap-28 mt-10 md:flex-nowrap md:h-screen md:-mt-16">
-    <div className="flex flex-col justify-center items-start gap-2 mx-auto md:w-full">
+    <div name="top" className="flex flex-wrap w-4/5 mx-auto gap-28 mt-10 md:flex-nowrap md:h-screen md:-mt-16">
+    <div className="flex flex-col justify-center items-start gap-2 md:w-full">
       <Fade left cascade>
         <div>
           <p className="text-accentColor text-sm tracking-widest">
