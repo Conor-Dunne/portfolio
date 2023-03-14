e33ad9cd50c60ffaadae14a2e81ffccde451c65e
Conor Dunne <conordunne@outlook.ie>
2023-03-14 12:05:46 +0900

    Add links to project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,31 +1,30 @@
-import Fade from 'react-reveal/Fade';
-import {FaGithub ,FaExternalLinkAlt} from "react-icons/fa"
+import Fade from "react-reveal/Fade";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
-function ProjectCard({project}) {
-    const {name, tools, description,image, live, code } = project;
-    return (
-        
-        <div className="flex flex-col justify-between gap-2 p-2 max-w-xs tracking-wide bg-secondaryColor shadow-black shadow-sm hover:scale-105 transition-all ease-in-out cursor-pointer">
-            <Fade>
-            <img src={image} className="rounded" />
-            <div>
-                <h2 className="text-2xl text-slate-300 font-bold">{name}</h2>
-            </div>
-            <div className="h-16 text-sm rounded">
-                {description}
-            </div>
-            <div className="font-mono text-accentColor">
-                {tools}
-            </div>
-            <div className='flex gap-5 justify-end'>
-                <FaGithub />
-                <FaExternalLinkAlt />
-            </div>
-            </Fade>
+function ProjectCard({ project }) {
+  const { name, tools, description, image, live, code } = project;
+  return (
+    <div className="flex flex-col justify-between gap-2 p-2 max-w-xs tracking-wide bg-secondaryColor shadow-black shadow-sm hover:scale-105 transition-all ease-in-out">
+      <Fade>
+        <a href={live} target="_blank" rel="noopener noreferrer">
+          <img src={image} className="rounded" />
+          <div>
+            <h2 className="text-2xl text-slate-300 font-bold">{name}</h2>
+          </div>
+          <div className="h-16 text-sm rounded">{description}</div>
+        </a>
+        <div className="font-mono text-accentColor">{tools}</div>
+        <div className="flex gap-5 justify-end">
+          <a href={code} target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+          <a href={live} target="_blank" rel="noopener noreferrer">
+            <FaExternalLinkAlt />
+          </a>
         </div>
-        
-    )
-  }
-  
-  export default ProjectCard
-  
+      </Fade>
+    </div>
+  );
+}
+
+export default ProjectCard;
