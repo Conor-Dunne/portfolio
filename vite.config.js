6d5b7585b8da5a670efe2f33973ef33beaa37c8e
Conor Dunne <conordunne@outlook.ie>
2023-06-28 00:45:06 +0000

    Update vite config for img opt

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name]-[hash][extname]',
+      },
+    },
+    terserOptions: {
+      compress: {
+        keep_infinity: true,
+        drop_console: true,
+      },
+    },
+  },
 })
